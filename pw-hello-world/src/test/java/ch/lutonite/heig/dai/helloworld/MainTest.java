852fab2148241e36544abf3e8a660de0a40dc20d
Loïc Herman <loic@loicherman.ch>
2023-09-21 20:14:01 +0200

    chore: add checkstyle configuration for project
    The checkstyle configuration is based on the Google rules, only adding
    double indentation (2 spaces to 4 spaces).

--- pw-hello-world/src/test/java/ch/lutonite/heig/dai/helloworld/MainTest.java
+++ pw-hello-world/src/test/java/ch/lutonite/heig/dai/helloworld/MainTest.java
@@ -2,7 +2,7 @@ package ch.lutonite.heig.dai.helloworld;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MainTest {
 
